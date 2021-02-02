fbf1820d807f571afb45ca77a15be8c2d834f855
tejasvi400 <76554073+tejasvi400@users.noreply.github.com>
2021-02-02 19:11:06 +0530

    The supply mission

---
+++ helicopter/project-23b-template-main/sketch.js
@@ -0,0 +1,76 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-15, width,10);
+	groundSprite.shapeColor=color(65)
+
+
+	engine = Engine.create();
+	world = engine.world;
+	side1= new Ground(410,650,200,20);
+	side2= new Ground(300,610,20,100);
+	side3= new Ground(500,600,20,100);
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.4, isStatic:true});
+	World.add(world, packageBody);
+	
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
+  side1.display();
+  side2.display();
+  side3.display();
+ 
+}
+
+function keyPressed() {
+	if (keyCode === RIGHT_ARROW) {
+		helicopterSprite.x+=25;
+		Matter.Body.translate(packageBody,{x:25,y:0});
+	}
+	if (keyCode === LEFT_ARROW) {
+		helicopterSprite.x-=25;
+		Matter.Body.translate(packageBody,{x:-25,y:0});
+	}
+
+ if (keyCode === DOWN_ARROW) {
+    Matter.Body.setStatic(packageBody,false);
+  }
+}
+
+
+
